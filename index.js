daf3a95bf576b5a688e79f5334567a7e6a85f7b2
Alex Garcia <alexsebastian.garcia@gmail.com>
2017-03-31 19:49:53 -0700

    changed port to listen to

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var app = express();
 app.use('/', require('./routes/normal.js'));
 app.use('/api', require('./routes/api.js'));
 
-app.listen(8000, function () {
-  console.log('Server listening on port 8000!')
+app.listen(process.env.PORT || 8000, function () {
+  console.log('Server listening success on ' + (process.env.PORT || 8000));
 });
